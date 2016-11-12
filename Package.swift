fca1beb185ed099096cd62b69c4ced6e37575c4e
Anthony Castelli <neueanthony@me.com>
2016-11-12 15:08:29 -0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "JWTAuthentication",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
+
