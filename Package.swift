f26326a3105bc651f21ec67ca9932e1f7abdb052
Anthony Castelli <neueanthony@me.com>
2017-05-31 13:21:49 -0700

    Add Auth and JWT Providers

--- Package.swift
+++ Package.swift
@@ -8,7 +8,9 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/jwt.git", majorVersion: 2)
     ],
     exclude: [
         "Config",
